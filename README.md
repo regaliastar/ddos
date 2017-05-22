101c30b001d49556e3f85f58c6d4828210679d73
regaliastar <1183080130@qq.com>
2017-05-22 17:13:08 +0800

    first

---
+++ README.md
@@ -0,0 +1,6 @@
+# ddos-UI
+
+## Lincense
+MIT
+
+> If you have any problem, please contact 1183080130@qq.com (ﾉﾟ▽ﾟ)ﾉ
---
+++ public/assets/js/downFlow.js
@@ -0,0 +1,107 @@
+"use strict";
+
+var passengerFlowData = [];
+/*
+ * 实时向服务器请求数据
+ * passengerFlowData : [] --> 用于存取数据 [['1997/07/06', 626],['1992/01/06',677]]
+ */
+
+
+
+/*
+ * 图表1
+ */
+// 基于准备好的dom，初始化echarts实例
+
+
+/****************************************
+ *
+ *
+ *
+ *****************************************/
+/*
+ * 图表2
+ */
+// 基于准备好的dom，初始化echarts实例
+var myChart3 = echarts.init(document.getElementById('realTimeDownFlow'));
+
+// 指定图表的配置项和数据
+function randomData2() {
+	now = new Date(+now + oneDay);
+	value = value + Math.random() * 21 - 10;
+	var result = {
+		name: now.toString(),
+		value: [
+            [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+            Math.round(value)
+        ]
+	};
+	//console.log(result);
+	return result;
+}
+
+var data = [];
+var now = +new Date(2015, 5, 22);
+var oneDay = 24 * 3600 * 1000;
+var value = Math.random() * 1000;
+for (var i = 0; i < 100; i++) {
+	data.push(randomData2());
+}
+
+var option = {
+	title: {
+		text: '实时上行流量动态折线图'
+	},
+	tooltip: {
+		trigger: 'axis',
+		formatter: function (params) {
+			//params = params[0];
+			var date = new Date(params[0].name);
+			return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params[0].value[1];
+		},
+		axisPointer: {
+			animation: false
+		}
+	},
+	xAxis: {
+		type: 'time',
+		splitLine: {
+			show: false
+		}
+	},
+	yAxis: {
+		type: 'value',
+		boundaryGap: [0, '100%'],
+		splitLine: {
+			show: true
+		}
+	},
+	series: [{
+		name: '实时上行流量数据',
+		type: 'line',
+		showSymbol: false,
+		hoverAnimation: false,
+		data: data,
+		lineStyle: {
+			normal: {
+				color: '#ED801C'
+			}
+		},
+    }]
+};
+
+setInterval(function () {
+	for (var i = 0; i < 5; i++) {
+		data.shift();
+		data.push(randomData2());
+	}
+
+	myChart3.setOption({
+		series: [{
+			data: data
+		}]
+	});
+}, 1000);
+
+// 使用刚指定的配置项和数据显示图表。
+myChart3.setOption(option);
